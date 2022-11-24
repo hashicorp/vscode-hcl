74944477a57882d49dd08d78255b0ec951c48cc2
Daniel Banck <daniel@dbanck.de>
2022-11-24 13:49:19 +0100

    Polyfill path module

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,7 @@ const webExtensionConfig = {
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
       assert: require.resolve('assert'),
+      path: require.resolve('path-browserify'),
     },
   },
   module: {
